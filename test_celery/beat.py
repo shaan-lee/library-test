82f953a6464e4413f39da1b889e8db6b5f5dfd12
shaan.lee <dev.sehyeon@gmail.com>
2023-09-22 13:14:48 +0900

    test celery beat

--- test_celery/beat.py
+++ test_celery/beat.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery.schedules import crontab
 from test_celery.test_celery import app
 
 
@@ -6,5 +6,9 @@ app.conf.beat_schedule = {
     "run-me-every-10-sec": {
         "task": "test_celery.test_celery.print_log",
         "schedule": 10.0,
-    }
+    },
+    "run-me-every-scheduled-time": {
+        "task": "teste_celery.test_celery.print_log",
+        "schedule": crontab(hour="*", minute=1),
+    },
 }
